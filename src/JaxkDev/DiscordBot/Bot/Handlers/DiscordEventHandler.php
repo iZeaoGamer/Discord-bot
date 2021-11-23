0349a6aed4ddb740cfcfd00eb930e3ff25f2a6d8
James Hayward <voidminerpocket@gmail.com>
2021-11-23 19:30:35 +0000

    Lint Cleaner #2

--- src/JaxkDev/DiscordBot/Bot/Handlers/DiscordEventHandler.php
+++ src/JaxkDev/DiscordBot/Bot/Handlers/DiscordEventHandler.php
@@ -376,12 +376,11 @@ array(5) {
         if($data instanceof DiscordMessage){
             $message = ModelConverter::genModelMessage($data);
         }else{
-
-            $message = ["message_id", "channel_id" => $data->channel_id,
-            "server_id" => $data->guild_id];
-
+            $message = [];
             foreach($data->ids as $id){  
           $message["message_id"] = $id;
+          $message["channel_id"] = $data->channel_id;
+          $message["server_id"] = $data->guild_id;
         }
     }
             $packet = new MessageBuilkDeletePacket($message);
