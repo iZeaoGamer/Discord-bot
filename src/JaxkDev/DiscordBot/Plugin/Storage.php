881c86386d5d33169ac2de30d5869c42a7167df2
James Hayward <voidminerpocket@gmail.com>
2021-11-24 07:48:57 +0000

    Reformatting #1

--- src/JaxkDev/DiscordBot/Plugin/Storage.php
+++ src/JaxkDev/DiscordBot/Plugin/Storage.php
@@ -237,7 +237,7 @@ class Storage{
             if (self::isThread($id)) {
                 return self::getThread($id);
             }
-            }
+        }
     //    }
         return $serverChannel;
     }
