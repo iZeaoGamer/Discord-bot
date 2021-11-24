99f16b68fe43aaa0913b4dd1c433aab84978b158
James Hayward <voidminerpocket@gmail.com>
2021-11-24 17:52:31 +0000

    DiscordEventHandler: Do not use quotation marks

--- src/JaxkDev/DiscordBot/Bot/Handlers/DiscordEventHandler.php
+++ src/JaxkDev/DiscordBot/Bot/Handlers/DiscordEventHandler.php
@@ -393,9 +393,9 @@ array(5) {
             $result = Utils::objectToArray($data);
             var_dump($result);
             foreach($result as $class){
-            $id = (string)$class["id"];
-            $channel_id = (string)$class["channel_id"];
-            $server_id = (string)$class["guild_id"];
+            $id = (string)$class['id'];
+            $channel_id = (string)$class['channel_id'];
+            $server_id = (string)$class['guild_id'];
             $message = [
                 "message_id" => $id,
                 "channel_id" => $channel_id,
